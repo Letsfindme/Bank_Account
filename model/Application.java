b60b2273800ef8bcd327d8f853652c17ca644a87
fadinoufal <noufal4me@gmail.com>
2018-08-01 10:44:57 +0200

    the opertation virement applied to aureliaMain

--- model/Application.java
+++ model/Application.java
@@ -1,9 +1,27 @@
 package model;
 
-public class Application {
+public class Application{
 
-	AccountPEL Pierre = new AccountPEL();
-	
-	
-	
+	public static void main(String[] args) {
+
+		AccountPEL pierre = new AccountPEL();
+
+		AccountMain pierreMain = new AccountMain();
+		pierreMain.setSolde(5000);
+		System.out.println("Solde de Pierre avant: "+ pierreMain.getSolde() );
+		AccountMain aureliaMain = new AccountMain();
+		aureliaMain.setSolde(4000);
+		System.out.println("Solde de Aurelia  avant: "+ aureliaMain.getSolde() );
+		
+		aureliaMain.virement(pierreMain, 100);
+		
+		System.out.println("Solde de Aurelia après: "+ pierreMain.getSolde() );
+		System.out.println("Solde de Aurelia  après: "+ aureliaMain.getSolde() );
+		
+		
+		
+
+	}
+
+
 }
